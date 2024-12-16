a8aeeaa0963f9c2b607dcea09542468e2b52c0fa
Iroda'M <irodamirzaahmadova@gmail.com>
2024-12-16 20:29:00 +0500

    Add files via upload

---
+++ PYTHON2/chiziqliMasala.py
@@ -0,0 +1,126 @@
+# 1-masala :
+# a = int (input("1-son : "))
+# b = int (input("2-son : "))
+# print(f"Natija : {a + b}")
+
+# 2-masala :
+# a = int (input("1-son : "))
+# b = int (input("2-son : "))
+# print(f"Natija : {a * b}")
+
+# 3-masala :
+# son = int (input ("Son kiriting :"))
+# print (f"{son} ni 7 ga bolgandagi qoldig'i {son % 7} ga teng")
+
+# 4-masala :
+# son = int (input ("2 xonali son kiriting : "))
+# on = son % 10
+# bir = son // 10 % 10
+# print (f"{on}{bir}")
+
+# 5-masala :
+# A = float (input("A kesma : "))  # A uzunligini kiritamiz
+# B = float (input("B kesma : "))  # B uzunligini kiritamiz
+# res = A // B
+# print (f"Natija : {res} marta")
+
+# 6-masala :
+# while True:
+#  ism = input ("Isim kiriting :")
+#  print (f"Salom {ism}")
+
+# 7-masala :
+# satr = input ("Satr kiriting : ")
+# index = len(satr)
+# print (f"a"+ satr + "a")
+
+# 8-masala :
+# satr = input ("Satr kiriting :")
+# natija = satr + 'siz'
+# print (natija)
+
+# 9-masala :
+# bino = int (input("Bino sonini kiriting : "))
+# qavat = int (input("Qavat sonini kiriting : "))
+# xona = int (input("Xona sonini kiriting : "))
+# parta = int (input("Parta sonini kiriting : "))
+# a = qavat * xona
+# b = a * parta 
+# c = b * bino
+# print (f"{bino} ta binodagi Jami partalar soni : {c} ta ")
+
+# 10-masala :
+# son = int (input ("Son kiriting : "))
+# print (son * son)
+
+# 11-masala :
+# kvadrat = int(input("Kvadratning tomonini kiriting : "))
+# peremetr = 4 * kvadrat 
+# s = kvadrat * kvadrat
+# print (f"Kvadratning peremetri {peremetr} ga teng . ")
+# print (f"Kvadratning yuzi {s} ga teng .")
+
+# 12-masala :
+# import math
+
+# radius = int (input("Aylananing radiusini kiriting : "))
+# diametr = 2 * radius
+# yuzi =  math.pi * radius * radius
+# uzunlik = 2 * math.pi * radius
+
+# print(f"Aylaning diametri: {diametr}")
+# print(f"Aylaning yuzi: {yuzi:.2f}")  
+# print(f"Aylaning uzunligi: {uzunlik:.2f}")
+
+# 13-masala : 
+# import math 
+# gradus = int (input ("Gradusni kiriting : "))
+# print(f"{gradus} gradus {gradus:.4f} radian ga teng.")
+
+# 14-masala
+# metr = float (input ("Metrni kiriting : "))
+# santimetr =  metr * 100
+# millimetr = santimetr *1000
+# kilometr =metr / 1000
+# print (f"{metr} metr ")
+# print (f"{santimetr} santimetr ")
+# print (f"{millimetr} millimetr ")
+# print (f"{kilometr} kilometr ")
+
+# 15-masala :
+# son = int (input ("3 xonali son kiriting :"))
+# bir = son % 10
+# on = son // 10 % 10
+# yuz = son // 100
+# a = yuz - 1 
+# b = on - 1
+# c = bir - 1
+# print (f"Natija : {a}{b}{c}")
+
+# 16 -masala :
+# a = int (input ("1-sonni kiritingg :"))
+# b = int (input ("2-sonni kiritingg :"))
+# a, b = b, a
+# print (f"A : {a}")
+# print (f"B : {b}")
+
+# 17-masala : 
+# a = int (input ("Son 1 : "))
+# b = int (input ("Son 2 : "))
+# c = int (input ("Son 3 : "))
+# a , b , c = b , c , a
+# print (f"A : {a}")
+# print (f"B : {b}")
+# print (f"C : {c}")
+
+# 18-masala :
+# A = float(input("A : "))
+# B = float(input("B : "))
+# C = float(input("C : "))
+
+# AB = abs(A - B)
+# AC = abs(A - C)
+# BC = abs(B - C)
+# print(f"AB kesmasining uzunligi: {AB}")
+# print(f"AC kesmasining uzunligi: {AC}")
+# print(f"BC kesmasining uzunligi: {BC}")
